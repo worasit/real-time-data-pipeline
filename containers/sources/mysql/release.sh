f59ebe7c59dee52d6949715aaedd10444c99c9e8
worasit daimongkol <worasit.dmk501@gmail.com>
2018-12-17 23:26:30 +0700

    version 0.0.5

--- containers/sources/mysql/release.sh
+++ containers/sources/mysql/release.sh
@@ -8,7 +8,7 @@ git pull
 docker build -t ${IMAGE_DB_NAME_WITH_LATEST} .
 docker tag ${IMAGE_DB_NAME_WITH_LATEST} ${IMAGE_DB_NAME_WITH_VERSION}
 
-docker push ${IMAGE_DB_NAME_WITH_LATEST}
+#docker push ${IMAGE_DB_NAME_WITH_LATEST}
 docker push ${IMAGE_DB_NAME_WITH_VERSION}
 
 git add -A
